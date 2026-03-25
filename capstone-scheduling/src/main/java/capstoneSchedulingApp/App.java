31c45d414afa1352cc53554c99dd9c059badfa62
Nico Bartello <ndb63@pitt.edu>
2026-03-25 15:51:21 -0400

    Merged Gui + Removed Target Folder

--- capstone-scheduling/src/main/java/capstoneSchedulingApp/App.java
+++ capstone-scheduling/src/main/java/capstoneSchedulingApp/App.java
@@ -3,7 +3,7 @@ package capstoneSchedulingApp;
 
 public class App {
     public static void main(String[] args) {
-        Parser.parseFile("schedule.db","capstone-scheduling/src/main/java/capstoneSchedulingApp/Instructor_Conseq_Classes.csv", ",");
-        Query.queryGenericInst("schedule.db");
+        //Parser.parseFile("schedule.db","capstone-scheduling/src/main/java/capstoneSchedulingApp/Instructor_Conseq_Classes.csv", ",");
+        //Query.queryGenericInst("schedule.db");
     }
 }
